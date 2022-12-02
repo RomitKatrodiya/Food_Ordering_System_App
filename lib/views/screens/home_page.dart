b25139cf0c61d33a9d91bcc490fae0beac44539e
Romit Katrodiya <romitkatrodiya2308@gmail.com>
2022-12-02 15:09:33 +0530

    first commit

---
+++ lib/views/screens/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import '../components/nevigation_bar_icon.dart';
+import 'cart_screen.dart';
+import 'favorite_screen.dart';
+import 'home_screen.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> buildScreens() {
+      return [
+        HomeScreen(),
+        CartScreen(),
+        FavoriteScreen(),
+      ];
+    }
+
+    List<PersistentBottomNavBarItem> navBarsItems() {
+      return [
+        navigationBarIcon(icon: Icons.home),
+        navigationBarIcon(icon: Icons.shopping_cart),
+        navigationBarIcon(icon: Icons.favorite_outlined),
+      ];
+    }
+
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: buildScreens(),
+      items: navBarsItems(),
+      navBarStyle: NavBarStyle.style10,
+      navBarHeight: MediaQuery.of(context).size.height * 0.09,
+    );
+  }
+}
